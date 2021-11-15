ff2cb0e910f0deb03b143f5b69cd77a6df1d67fd
JGilbert <j.gilbert@mail.utoronto.ca>
2021-11-15 11:55:18 -0500

    Ingredient and dish creation tests

---
+++ src/main/test/IngredientTest.java
@@ -0,0 +1,27 @@
+import Entities.Ingredient;
+import EntityCreation.EntityCreatorDistributor;
+import EntityCreation.IngredientCreation.IngredientCreator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTest {
+    Ingredient ing = new Ingredient();
+    IngredientCreator IC;
+
+    @Before
+    public void setup() {
+        ing.addName("pepperoni");
+        ing.addAttribute("NutFree");
+    }
+
+    @Test(timeout = 100)
+    public void fileINGtest() {
+        IC = new IngredientCreator("pepperoni@NutFree");
+        Ingredient ret = IC.create();
+        assertEquals(ing.name, ret.name);
+        assertEquals(ing.attributes, ret.attributes);
+    }
+
+}
