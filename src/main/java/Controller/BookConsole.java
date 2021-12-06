e739ba40af19134d0c706a62e2f9675fbc47d56d
PSNPT <prabuddha.senapati@mail.utoronto.ca>
2021-12-05 21:10:35 -0500

    Added DB removal for User,Dish,Ingredient and Updating of Favourites and Preferences for User

--- src/main/java/Controller/BookConsole.java
+++ src/main/java/Controller/BookConsole.java
@@ -5,6 +5,7 @@ import ObjectConversion.ReferenceStorage;
 import Presenter.BookView;
 import Presenter.OpenSesame;
 import Search.Search;
+import Serialization.AddToDB;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -39,12 +40,14 @@ public class BookConsole implements AbstractConsole {
                 String dishName1 = sc.nextLine().toLowerCase();
                 Dish d1 = ReferenceStorage.dm.nameToDish(dishName1);
                 ReferenceStorage.u.addFavourite(d1);
+                new AddToDB().SetFavourites(ReferenceStorage.u);
                 break;
             case "remove from favourites" :
                 System.out.println("Which dish would you like to remove from favourites?");
                 String dishName2 = sc.nextLine().toLowerCase();
                 Dish d2 = ReferenceStorage.dm.nameToDish(dishName2);
                 ReferenceStorage.u.removeFavourite(d2);
+                new AddToDB().SetFavourites(ReferenceStorage.u);
                 break;
             case "preferences" : BookView.view((ArrayList<Dish>) GetExecutor.grab(prefDishes)); //preferenceSearch
                 break;
